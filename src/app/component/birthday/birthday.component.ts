c83a2763d2363d08a1e7c5971f2ce99a4537ef87
Sufail Badusha <sufailbadushat@gmail.com>
2023-12-03 21:27:14 +0530

    get birth day emp add

--- src/app/component/birthday/birthday.component.ts
+++ src/app/component/birthday/birthday.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-birthday',
   templateUrl: './birthday.component.html',
   styleUrls: ['./birthday.component.css']
 })
-export class BirthdayComponent {
+export class BirthdayComponent implements OnInit{
 
+  constructor(private api:ApiService) {}
+
+  data:any = [];
+
+  ngOnInit(){
+    this.api.getBirthdayEmp().subscribe((res) => {
+      this.data=res;
+    })
+  }
 }
